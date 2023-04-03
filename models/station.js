699a1d74e507c1196b46dba32e6acb89c68e853d
jrh1010101101010 <jhamblin798@gmail.com>
2023-04-03 15:08:32 +1000

    added all json

--- models/station.js
+++ models/station.js
@@ -2,18 +2,11 @@ const db = require('../db')
 
 class Station {
 
-
-
-
-
-
-
-
-
-
-
-
-
+    static getAll (){
+        let sql = `select * from service_stations limit 400;`
+    
+        return db.query(sql)
+    }
 
 
 
