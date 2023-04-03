cc04672d6dbf2f516b1e914d09cd4806d0167c0a
henry-rennell <henryrennell@gmail.com>
2023-04-03 12:07:50 +1000

    working map and html skeleton

---
+++ db/seed_petrol_stations.js
@@ -0,0 +1,15 @@
+// const db = require('./index')
+// const fs = require('fs');
+
+
+// const stationData = fs.readFileSync('./service_stations.txt', {encoding:'utf8', flag:'r'}).split('\n')
+
+// db.query('select * from service_stations;', (err, res) => {
+//     console.log(res);
+// });
+
+
+
+
+
+
