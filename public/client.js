c87277975b8f350153b0368d624e1b03e63a06b5
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-04-03 14:42:38 +0700

    update long and lat for sydney

--- public/client.js
+++ public/client.js
@@ -6,7 +6,7 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -33.865143, lng: 151.209900 },
     zoom: 13,
     minZoom: 10
   });
