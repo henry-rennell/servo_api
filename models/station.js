ede068d32eb6e7508c6a288e655007558c066e35
henry-rennell <henryrennell@gmail.com>
2023-04-03 16:23:08 +1000

    refactoring

--- models/station.js
+++ models/station.js
@@ -8,6 +8,14 @@ class Station {
         return db.query(sql)
     }
 
+    static getOwners(){
+    let sql = 'select distinct owner from service_stations;'
+    return db.query(sql).then(result => {
+        return result.rows.map(row => row.owner)
+      })
+    }
+    
+
 
 
 
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express()
 const config = require("./config")
 const expressLayouts = require("express-ejs-layouts")
 const Station = require('./models/station')
-const db = require('./db/index')
-
 const db = require('./db')
 
 app.set("view engine", "ejs")
@@ -19,19 +17,12 @@ app.get("/", (req, res) => {
 })
 
 app.get('/api/owners', (req, res) => {
-  const sql = 'select distinct owner from service_stations;'
-
-  db.query(sql).then(result => {
-    const owners = result.rows.map(row => row.owner)
-    console.log(owners)
-    res.json(owners)
-  })
+  Station.getOwners().then(owners => res.json(owners))
+})
+app.get('/api/station/all', (req,res) => {
+    Station.getAll().then(dbres => res.json(dbres.rows))
 })
 
 app.listen(config.port, () => {
   console.log(`listening on port ${config.port}`)
-})
-
-app.get('/api/station/all', (req,res) => {
-    Station.getAll().then(dbres => res.json(dbres.rows))
 })
