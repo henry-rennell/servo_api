c7671aa6b80800d0fa9a782298e971c6d2a4e6e2
henry-rennell <henryrennell@gmail.com>
2023-04-04 15:34:42 +1000

    on drag-end, fetch markers

--- public/client.js
+++ public/client.js
@@ -15,8 +15,9 @@ async function initMap(location) {
     zoom: 13,
     minZoom: 10
   });
-  //getting locations to add map markers
-  axios.get('/api/stations/all')
+  map.addListener('dragend', () => {
+
+    axios.get('/api/stations/all')
     .then(res => res.data.forEach(location => {
       let latLong = {lat: location.lat, lng: location.long}
       let marker = new google.maps.Marker({
@@ -41,8 +42,11 @@ async function initMap(location) {
               marker.addListener("click", () => {
                 infoWindow.open(map, marker);
               });
+
       marker.setMap(map)
     }))
+  })
+  //getting locations to add map markers
 }
 
 //function to handle user allowing geolocation
