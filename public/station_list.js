4015b986fb542d3621891311e9d8d0bddc1bd2f9
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-04-04 09:38:03 +0700

    stats box part2 and fix bug

--- public/station_list.js
+++ public/station_list.js
@@ -1,16 +1,14 @@
 const nearest = document.querySelector('.nearest')
 
 function renderStationList(stations) {
-    nearest.innerHTML = stations.map((station) => renderStation(station)).join(',')
+    nearest.innerHTML = stations.map((station) => renderStation(station)).join('')
 }
 
 function renderStation(station) {
-    return `
-     <p>${station.name} ${station.owner} ${station.address}<p>
-    `
+    return `<p>${station.name} ${station.owner} ${station.address}<p>`
 }
 
 axios.get('/api/stations/all').then(result => {
-    console.log(result)
-    return result.data.slice(0, 9)
+    console.log(result.data.slice(0, 10))
+    return result.data.slice(0, 10)
 }).then(renderStationList)
