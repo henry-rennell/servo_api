fb750538463d2adc9f2cc18261a884eb6febe4bf
jrh1010101101010 <jhamblin798@gmail.com>
2023-04-05 10:34:47 +1000

    fixed spotlight

--- public/spotlight_info.js
+++ public/spotlight_info.js
@@ -1,13 +1,17 @@
+import { map } from "./client.js"
+
 // createing the random pertrol station endpoint
 const spotlightInfo = document.querySelector('.spotlight-station')
 
 function renderSpotlight (station){
    spotlightInfo.innerHTML = htmlElementRan(station)
+   document.querySelector('.random-station').addEventListener('click', () => {
+      centerMapOnStation(station.lat, station.long)})
 }
 
 function htmlElementRan (station){
     return `
-            <p><a href="#" onclick="centerMapOnStation(${station.lat}, ${station.long}); return false;">${station.name}</a></p> 
+            <p><a href="#" class="random-station"> ${station.name}</a></p>
             <p>${station.owner}<p>
             <p>${station.address}<p>`
 }
@@ -23,9 +27,11 @@ function handleRefresh (event){
    axios.get("http://localhost:8080/api/stations/random").then(result => result.data[0]).then(data => renderSpotlight(data))
 }
 
+//const ranStation = document.querySelector('.random-station')
+
+//ranStation.addEventListener
+
+
 function centerMapOnStation(latitude, longitude) {
-   const map = new google.maps.Map(document.getElementById("map"), {
-     center: { lat: latitude, lng: longitude },
-     zoom: 15,
-   });
- }
+   map.setCenter({lat: latitude, lng: longitude  })
+ } 
